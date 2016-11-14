a4c9f26bef3c6fa62256e2a424ae26ae615280bc
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2016-11-14 16:45:43 -0500

    Virginia Gateway 1.0  /site.js

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
 var site_json = {
                     "mall_name" : "Virginia Gateway",
-                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/56c740936e6f642d56000000/image/png/1455899596000/main_logo.png",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/581107b46e6f642cc0000000/image/png/1479159913000/valogo copy.png",
                     "time_zone" : "US/Eastern",
                     "newsletter_form_url" : "http://mobilefringe.createsend.com/t/d/s/bitjjd/?callback=?",
                     "newsletter_email_field" : "cm-bitjjd-bitjjd"
