17ffb8951629efd557763cf65860db350f18f7d4
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2017-04-07 12:45:52 -0400

    Virginia Gateway 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -319,6 +319,7 @@ function submit_contest(data) {
 function renderSitePopup(){
     var pp = getPopups()[0];
     if (pp != undefined){
+        log(pp.contest.name != undefined )
         if (pp.contest.name != undefined && pp.contest.name.length > 0){
             $('.popup-body').append('<img id="pp_img" class="pp_img_bg" src="http://cdn.mallmaverick.com' + pp.photo_url + '" alt="pop up">');
             $('.popup-close-form').click(function(){
