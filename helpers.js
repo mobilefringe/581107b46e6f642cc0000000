96145e3f4cbb3452bb0d6bce36153351a314dd28
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2017-04-07 11:55:29 -0400

    Virginia Gateway 1.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -319,13 +319,15 @@ function submit_contest(data) {
 function renderSitePopup(){
     var pp = getPopups()[0];
     if (pp != undefined){
-        if (pp.contest.length > 0){}
+        if (pp.contest.length > 0){
+            $('.popup-body').html('<img id="pp_img" src="http://cdn.mallmaverick.com' + pp.photo_url + '" alt="pop up">')
+        }
         else{
             if (pp.photo_link.length > 0){
                 $('.popup-body').html('<a id="pp_url" href="' + pp.photo_link +'"><img id="pp_img" src="http://cdn.mallmaverick.com' + pp.photo_url + '" alt="pop up"></a>')
             }
             else{
-                $('.popup-body').html('<img id="pp_img" src="http://cdn.mallmaverick.com' + pp.photo_url + '" alt="pop up">')
+                
             }
         }
     
